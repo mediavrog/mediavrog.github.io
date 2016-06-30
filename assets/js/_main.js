7203b8e415e6f6053585f5ab51ebed07d112be02
Maik Vlcek <maik@mediavrog.net>
2016-06-30 17:35:05 +0900

    basic webmention support

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -4,6 +4,26 @@
 
 $(document).ready(function(){
 
+  // webmention.io
+  //var urls = [];
+  //var base;
+  //
+  //$("*[data-webmention-count]").each(function(i,e){
+  //  var parser = document.createElement('a');
+  //  parser.href = $(e).data('url');
+  //  base = parser.protocol + "//" + parser.hostname;
+  //  urls.push(parser.pathname+parser.search);
+  //});
+  //
+  //$.getJSON("https://webmention.io/api/count?jsonp=?", {
+  //  base: base,
+  //  targets: urls.join(",")
+  //}, function(data){
+  //  $("*[data-webmention-count]").each(function(i,e){
+  //    $(e).text(data.count[$(e).data('url')]);
+  //  });
+  //});
+
   // FitVids init
   $("#main").fitVids();
 
